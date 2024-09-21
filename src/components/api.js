9683f10ac321ae906cf5d3b0ead7d4b8ff5b9f2e
Dhyaneshvar Prasaath J M <140618257+Dhyanesh02@users.noreply.github.com>
2024-09-21 17:02:32 +0530

    Delete src directory

--- src/components/api.js
+++
@@ -1,9 +0,0 @@
-export const geoApiOptions = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'f63f3e2ef1mshdbdc473572d3c9ep11ed84jsn5ef4e30fd4b2',
-		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
-	}
-};
-
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
